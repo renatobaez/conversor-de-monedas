dd2bb4eca5d8758ab5afb703085e4646eae4662e
renatobaez <panchobaez@gmail.com>
2023-08-30 17:52:10 -0400

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,45 @@
 
-const selectIndicadores = document.getElementById("indicadores")
+const selectIndicators = document.getElementById("indicators")
+const btnSearch = document.querySelector("button")
+const txtCLP = document.querySelector("input")
+const lblResult = document.querySelector("#result")
 const apiURL = "https://mindicador.cl/api/"
+const indicatorsJson = [];
+const nf = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 2, roundingIncrement: 5 })
 
 const getData = async (url) => {
-  const res = await fetch(url)
-  return await res.json()
+  try{
+    const res = await fetch(url)
+    return await res.json()
+  } catch (error){
+    console.error(error)
+  }
 }
 
-const getIndicadores = async () => {
-  try{
+const getIndicators = async () => {
     const results = await getData(apiURL)
     for (let i in results) {
       if(typeof results[i] === "object"){
         let option = document.createElement("option")
         option.value = results[i].codigo
         option.text = results[i].nombre
-        selectIndicadores.add(option)
+        selectIndicators.add(option)
+        indicatorsJson.push({code: results[i].codigo, date: results[i].fecha, value: results[i].valor})
       }
     }
-  } catch (error){
-    console.error(error)
-  }
 }
 
-getIndicadores()
+getIndicators()
+console.log(indicatorsJson);
+btnSearch.addEventListener("click", () => {
+  if(txtCLP.value === ""){
+    alert("Debes ingresar el mondo en CLP")
+    txtCLP.focus()
+  }else if(selectIndicators.value === ""){
+      alert("Debes seleccionar una moneda a convertir")
+      selectIndicators.focus()
+  }else{
+    const indexIndicators = indicatorsJson.findIndex( searchIndex => searchIndex.code === selectIndicators.value)
+	  lblResult.textContent = "Resultado: " + nf.format(txtCLP.value / indicatorsJson[indexIndicators].value)
+  }
+})
