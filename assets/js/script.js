3c5d506cef3d8e6fcc8fb48d41ecca72d33f5e76
renatobaez <panchobaez@gmail.com>
2023-09-01 11:50:12 -0400

    final commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,64 +4,45 @@ const btnSearch = document.querySelector("button")
 const txtCLP = document.querySelector("input")
 const lblResult = document.querySelector("#result")
 const indicatorChart = document.getElementById("indicatorChart")
-let myChart = ""
-const apiURL = "https://mindicador.cl/api/"
 const indicatorsJson = [];
-
-
+const apiURL = "https://mindicador.cl/api/"
 const nf = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 2, roundingIncrement: 5 })
+let myChart = ""
 
-const getData = async (url) => {
+async function getData(api){
   try{
-    const res = await fetch(url)
+    const res = await fetch(api)
+    if (!res.ok){
+      throw new Error(res.statusText)
+    }
     return await res.json()
-  } catch (error){
-    console.error(error)
+  }catch (error){
+    lblResult.textContent = error
+    return null
   }
 }
 
-const getIndicators = async () => {
+async function getIndicators(){
     const results = await getData(apiURL)
-    for (let i in results) {
-      if(typeof results[i] === "object"){
-        let option = document.createElement("option")
-        option.value = results[i].codigo
-        option.text = results[i].nombre
-        selectIndicators.add(option)
-        indicatorsJson.push({code: results[i].codigo, date: results[i].fecha, value: results[i].valor})
+    if(results) {
+      for (let i in results) {
+        if(typeof results[i] === "object"){
+          let option = document.createElement("option")
+          option.value = results[i].codigo
+          option.text = results[i].nombre
+          selectIndicators.add(option)
+          indicatorsJson.push({code: results[i].codigo, date: results[i].fecha, value: results[i].valor})
+        }
       }
     }
 }
 
-getIndicators()
-
-btnSearch.addEventListener("click", () => {
-  if(txtCLP.value === ""){
-    alert("Debes ingresar el mondo en CLP")
-    txtCLP.focus()
-  }else if(selectIndicators.value === ""){
-      alert("Debes seleccionar una moneda a convertir")
-      selectIndicators.focus()
-  }else{
-    const indexIndicators = indicatorsJson.findIndex( searchIndex => searchIndex.code === selectIndicators.value)
-	  lblResult.textContent = "Resultado: " + nf.format(txtCLP.value / indicatorsJson[indexIndicators].value)
-    renderChart()
-  }
-})
-
-async function getDataToChart() {
-  try{
-    const res = await fetch(apiURL + selectIndicators.value)
-    return await res.json()
-  } catch (error){
-    console.error(error)
-  }
-}
-async function renderChart() {
-  const data = await getDataToChart()
+async function renderChart(){
+  const data = await getData(apiURL + selectIndicators.value)
   const size = 10;
   const labels = data.serie.slice(0, size).map(i => {
-    return i.fecha.slice(0,10)
+    const formatDate = i.fecha.slice(0,10).split("-")
+    return formatDate[2]+"-"+formatDate[1]+"-"+formatDate[0]
   })
   const values = data.serie.slice(0, size).map(i => {
     return i.valor
@@ -85,4 +66,18 @@ async function renderChart() {
   })
 }
 
+btnSearch.addEventListener("click", () => {
+  if(txtCLP.value === ""){
+    lblResult.textContent = "Debes ingresar el mondo en CLP"
+    txtCLP.focus()
+  }else if(selectIndicators.value === ""){
+    lblResult.textContent ="Debes seleccionar una moneda a convertir"
+    selectIndicators.focus()
+  }else{
+    const indexIndicators = indicatorsJson.findIndex( searchIndex => searchIndex.code === selectIndicators.value)
+	  lblResult.textContent = "Resultado: " + nf.format(txtCLP.value / indicatorsJson[indexIndicators].value)
+    renderChart()
+  }
+})
 
+getIndicators()
